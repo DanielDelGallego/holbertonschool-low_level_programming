6c1605ef2e378715cbc881cc1df4c52172e80335
Daniel Del Gallego <1798@holbertonschool.com>
2020-06-10 23:26:52 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Description.
+ *
+ * Return: Always (0).
+ */
 
 int main(void)
+
 {
+
 puts("\"Programming is like building a multilingual puzzle");
 
 return (0);
+
 }
