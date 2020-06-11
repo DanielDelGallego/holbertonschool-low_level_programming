8ed4402be2cc3486c56df819e5463ce06e2ed7fb
Daniel Del Gallego <1798@holbertonschool.com>
2020-06-11 00:29:51 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Description.
+ *
+ * Return: Always (0).
+ */
+
+int main(void)
+
+{
+
+Printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
+}
